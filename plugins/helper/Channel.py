a8e17b55d6bf8a14c9414935f63606d5565989a2
Kushal HK <115166395+Kushalhk@users.noreply.github.com>
2024-11-08 10:40:59 +0530

    Update Channel.py

--- plugins/helper/Channel.py
+++ plugins/helper/Channel.py
@@ -9,10 +9,8 @@ CHANNEL_ID = -1002459409813
 async def add_button(client, message):
     if message.chat.id == CHANNEL_ID:
         button = InlineKeyboardMarkup(
-            [[
-              InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/+J5zlO5boIu9mZGY1"),       
-           ],[
-              InlineKeyboardButton("🎵𝗞𝗮𝘃𝗶𝘁𝗵𝗮𝗶 𝗔𝗻𝗱 𝗠𝘂𝘀𝗶𝗰 𝗦𝗼𝘂𝗹🎵", url="https://whatsapp.com/channel/0029Va5rjzvBVJlCi31Gg90N"))]]
+            [[InlineKeyboardButton("🔰𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽🔰", url="https://t.me/+J5zlO5boIu9mZGY1")]]
+        )
         
         try:
             # Try to add the button to the message
